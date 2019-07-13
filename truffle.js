4d3740ee22ff260576b110cc7e217385da4b2c99
Matas <mempakeris@me.com>
2019-07-12 19:38:40 -0700

    made changes to config file in order to work with truffle 5 and solc 0.4.24

--- truffle.js
+++ truffle.js
@@ -3,7 +3,13 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
+      websockets: true,
       network_id: "*" // Match any network id
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.24"
     }
   }
 };
